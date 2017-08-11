ff5b73cf926293894fd17637cbada06954a8294f
beth97209@gmail.com <Guest@Epicodus-7G.local>
2017-08-11 12:27:41 -0700

    add updated arraylist in team.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +8,21 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/", (request, response) -> "Hello Friend!"); //spark route
+       staticFileLocation("/public");
+
+        get("/posts/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "add-team-member-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/posts/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(req.params("id"));
+            Team foundPost = Team.findById(idOfPostToFind);
+            model.put("post", foundPost);
+            return new ModelAndView(model, "post-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
     }
 }
--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -2,23 +2,97 @@ package models;
 
 import java.util.ArrayList;
 
-/**
- * Created by Guest on 8/11/17.
- */
+
 public class Team {
 
-    private final String content;
-    private static ArrayList<Team> instances = new ArrayList<>();
+    private String member1;
+    private String member2;
+    private String member3;
+    private String member4;
+    private int id;
+    private boolean published;
+    private static ArrayList<Team> memberList = new ArrayList<>();
 
-    public Team(String content){
-        this.content = content;
+    public Team(String member1, String member2, String member3, String member4){
+        this.member1 =member1;
+        this.member2 = member2;
+        this.member3 = member3;
+        this.member4 = member4;
+        this.published = false;
+        memberList.add(this);
+        this.id = memberList.size();
     }
 
-    public String getContent() {
-        return content;
+    public ArrayList<Team> getContent(){
+        return memberList;
+    }
+    public void update(String member1, String member2, String member3, String member4){
+        this.member1 = member1;
+        this.member2 = member2;
+        this.member3 = member3;
+        this.member4 = member4;
     }
 
-    public static ArrayList<Team> getAll(){
-        return instances;
+    public static Team findById(int id){
+        return memberList.get(id-1);
     }
+
+    public static ArrayList<Team> getAll() {
+        return memberList;
+    }
+
+    // test-clear-all-method//
+
+    public static void clearAllPosts(){
+        memberList.clear();
+    }
+    //                     //
+
+
+    public boolean getPublished(){
+        return this.published;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    /// getter methods ///
+    //////////////////////
+
+    public String getMember1() {
+        return member1;
+    }
+    public String getMember2() {
+        return member2;
+    }
+    public String getMember3() {
+        return member3;
+    }
+    public String getMember4() {
+        return member4;
+    }
+    public static ArrayList<Team> getMemberList() {
+        return memberList;
+    }
+    ////////////////////////
+    ////////////////////////
+
+
+
+
+//    private final String content;
+//    private static ArrayList<Team> instances = new ArrayList<>();
+//
+//    public Team(String content){
+//        this.content = content;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public static ArrayList<Team> getAll(){
+//        return instances;
+//    }
 }
